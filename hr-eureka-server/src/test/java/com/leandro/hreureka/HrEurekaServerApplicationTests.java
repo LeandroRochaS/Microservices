810737728c8ec61dc8bf54248802754a4ad0b311
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-20 18:15:28 -0300

    Create Eureka

---
+++ hr-eureka-server/src/test/java/com/leandro/hreureka/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
