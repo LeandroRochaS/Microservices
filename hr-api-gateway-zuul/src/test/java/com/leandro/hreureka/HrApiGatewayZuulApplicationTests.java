c43792fc3f358e198a5c88237fab2d19c6414676
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-21 11:26:45 -0300

    Create Api-gateway-zuul

---
+++ hr-api-gateway-zuul/src/test/java/com/leandro/hreureka/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
