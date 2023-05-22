10518e32828608356210f9e66c8c6219556d5c96
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-22 17:46:44 -0300

    User service create

--- hr-eureka-server/src/main/java/com/leandro/hreureka/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/leandro/hreureka/HrEurekaServerApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication	
+
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
