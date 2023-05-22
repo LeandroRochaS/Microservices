e939db15a0367559946eeb57f58bfb67afa002a4
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-21 21:37:32 -0300

    Create Project

---
+++ hr-config-server/src/test/java/com/leandro/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
