1d996fbb3485f1bfa7f919bff3853a81d8d5890b
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-22 22:48:39 -0300

    Project hr-oauth created

---
+++ hr-user/src/main/java/com/leandro/hruser/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.leandro.hruser.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.leandro.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
