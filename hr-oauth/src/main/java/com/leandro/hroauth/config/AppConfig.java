c49e58177e0e6174290a14d6f9820e29087b8d77
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-24 20:00:37 -0300

    Implementation JWT 1

--- hr-oauth/src/main/java/com/leandro/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/leandro/hroauth/config/AppConfig.java
@@ -1,22 +1,37 @@
 package com.leandro.hroauth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @EnableWebSecurity
-
 public class AppConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		return tokenConverter;
+	}
 
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
 
-@Override
-
-public void configure(HttpSecurity http) throws Exception {
-
-    // @formatter:off
+	// @formatter:off
 
     http
 
@@ -38,6 +53,6 @@ public void configure(HttpSecurity http) throws Exception {
 
     // @formatter:on
 
-}
+	}
 
 }
